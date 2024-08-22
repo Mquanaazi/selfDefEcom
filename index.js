1e0991ab09fbeaf5c29e9aed5030db6a792fe71d
mquanaazi <olwethumalumla31@gmail.com>
2024-08-22 16:43:05 +0200

    hehre

--- index.js
+++ index.js
@@ -5,13 +5,20 @@ import cors from "cors";
 
 const app=express()
 let port=process.env.PORT
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Credentials", "true");
+    res.header("Access-Control-Allow-Methods", "*");
+    res.header("Access-Control-Request-Methods", "*");
+    res.header("Access-Control-Allow-Headers", "*");
+    res.header("Access-Control-Expose-Headers", "Authorization");
+    next()
+})
+app.use(cors())
 app.use(express.json())
-app.use(cors({origin:'http://localhost:8081',credentials:true}))
-
-
+app.use(express.static('public'))
 app.use('/products',productsRoutes)
 app.use( '/users',usersRoutes)
-app.use(express.static('public'))
 app.listen(port,()=>{
     console.log(`server is running on port ${port}`)
     
